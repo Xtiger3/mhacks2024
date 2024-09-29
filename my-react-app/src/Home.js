cc5ba54e7e4a3ab6b62a92743cc9a49f9f011031
Sonia <sonialin@umich.edu>
2024-09-28 21:26:34 -0400

    merge again

--- my-react-app/src/Home.js
+++ my-react-app/src/Home.js
@@ -3,9 +3,13 @@
     import List from './List';
     import ProgressDashboard from './ProgressDashboard'; 
     import Chatbot from './Chatbot';
+import './App.css';
+import Papa from 'papaparse';
+
 
     const Home = () => {
         const [recentSubmissions, setRecentSubmissions] = useState([]);
+    const [similarQuestions, setSimilarQuestions] = useState([]);
         const [upNext, setUpNext] = useState([]);
         const [solvedProblems, setSolvedProblems] = useState(0);
         const [totalProblems, setTotalProblems] = useState(0);
@@ -17,6 +21,35 @@
         const [totalHard, setTotalHard] = useState(0);
         const [numDaysSinceLast, setNumDaysSinceLast] = useState(0);
         const username = localStorage.getItem('leetcodeUsername');
+
+    
+
+    const getSimilarQuestions = async () => {
+        const similarQuestionsSet = new Set();
+        Papa.parse('/problems.csv', {
+            download: true,
+            header: true,
+            complete: (results) => {
+                results.data.forEach((row) => {
+                    if (row.title !== undefined) {
+                        let title = (row.title).split('. ')[1]
+                        if (recentSubmissions.includes(title)) {
+                            // const parsedArray = JSON.parse(row.similar_questions.replace(/""/g, '"')); // Replaces the double quotes with single ones
+                            const trimmedString = row.similar_questions.replace(/[\[\]'"]/g, '');
+                            const questions = trimmedString.split(',');
+                            questions.forEach((question) => {
+                            similarQuestionsSet.add(question.trim());
+                            });
+                        }
+                    }
+                            
+                });
+    
+                // Get the top 3 unique similar questions and resolve the promise
+                setSimilarQuestions(Array.from(similarQuestionsSet).slice(0, 3));
+            },
+        });
+    }
 
         const daysAgo = (timestamp) => {
             const now = Date.now();
@@ -31,8 +64,9 @@
                     const response = await fetch('https://leetcode-api-faisalshohag.vercel.app/' + username);
                     const data = await response.json();
                     console.log(data.recentSubmissions.slice(0, 3));
-                    
-                    setRecentSubmissions(data.recentSubmissions.slice(0, 3));
+                    const uniqueTitles = new Set(data.recentSubmissions.map((submission) => submission.title));
+                const submissionTitles = Array.from(uniqueTitles).slice(0, 3);
+                    setRecentSubmissions(submissionTitles);
                     setSolvedProblems(data.totalSolved);
                     setTotalProblems(data.totalQuestions);
                     setSolvedEasyProblems(data.easySolved);
@@ -41,6 +75,31 @@
                     setTotalEasy(data.totalEasy);
                     setTotalMed(data.totalMedium);
                     setTotalHard(data.totalHard);
+
+                const similarQuestionsSet = new Set();
+                Papa.parse('/problems.csv', {
+                    download: true,
+                    header: true,
+                    complete: (results) => {
+                        results.data.forEach((row) => {
+                            if (row.title !== undefined) {
+                                let title = (row.title).split('. ')[1]
+                                if (submissionTitles.includes(title)) {
+                                    // const parsedArray = JSON.parse(row.similar_questions.replace(/""/g, '"')); // Replaces the double quotes with single ones
+                                    const trimmedString = row.similar_questions.replace(/[\[\]'"]/g, '');
+                                    const questions = trimmedString.split(',');
+                                    questions.forEach((question) => {
+                                    similarQuestionsSet.add(question.trim());
+                                    });
+                                }
+                            }
+                                    
+                        });
+            
+                        // Get the top 3 unique similar questions and resolve the promise
+                        setSimilarQuestions(Array.from(similarQuestionsSet).slice(0, 3));
+                    },
+                });
                     if (data.recentSubmissions.length > 0) {
                         setNumDaysSinceLast(daysAgo(data.recentSubmissions[0].timestamp));
                     }
@@ -48,8 +107,8 @@
                     console.error("Error fetching data:", error);
                 }
             };
-
             fetchSubmissions();
+        // getSimilarQuestions();
         }, [username]);
 
         return (
@@ -59,6 +118,7 @@
                     <div style={{ flex: '1', marginRight: '10px' }}> {/* List takes up 50% */}
                         <p>last submitted {numDaysSinceLast} days ago</p>
                         <List title="Recent Submissions" data={recentSubmissions} />
+                    <List title="Up Next" data={similarQuestions} />
                     </div>
                     <div style={{ flex: '1', marginLeft: '10px' }}> {/* ProgressDashboard takes up 50% */}
                         <ProgressDashboard 
