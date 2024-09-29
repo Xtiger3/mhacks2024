b8a409ffd7e78fbc24b936bc4940c6f4420cc844
Sonia <sonialin@umich.edu>
2024-09-29 10:40:17 -0400

    change logo

--- my-react-app/src/UsernameInput.js
+++ my-react-app/src/UsernameInput.js
@@ -14,7 +14,8 @@ const UsernameInput = () => {
 
     return (
         <div className="login-container" style={{ backgroundColor: "#1C254E" }}>
-            <h1>LeetRizz</h1>
+            {/* <h1>LeetRizz</h1> */}
+            <img src="/logo.png" style={{width:'20%'}}/>
             <p className="login-prompt">Enter your LeetCode username</p>
             <form onSubmit={handleSubmit}>
                 <input
